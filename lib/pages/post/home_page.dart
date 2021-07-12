0ab6fa5d42b2b10944a6f8d8a8d7dcbe8e67d992
Donghyeok Jang <ssjf409@naver.com>
2021-07-13 00:54:50 +0900

    add drawer

--- lib/pages/post/home_page.dart
+++ lib/pages/post/home_page.dart
@@ -1,11 +1,56 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_blog/size.dart';
 
 class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      drawer: _navigation(context),
+      appBar: AppBar(),
       body: Center(
         child: Text("Home Page"),
+      ),
+    );
+  }
+
+  Widget _navigation(BuildContext context) {
+    return Container(
+      width: getDrawerWidth(context),
+      height: double.infinity,
+      color: Colors.white,
+      child: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              TextButton(
+                onPressed: () {},
+                child: Text(
+                  '회원정보보기',
+                  style: TextStyle(
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.black54,
+                  ),
+                ),
+              ),
+              Divider(),
+              TextButton(
+                onPressed: () {},
+                child: Text(
+                  '로그아웃',
+                  style: TextStyle(
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.black54,
+                  ),
+                ),
+              ),
+              Divider(),
+            ],
+          ),
+        ),
       ),
     );
   }
