a2e83befe666d01fb498a99617ef61167650e4ea
Donghyeok Jang <ssjf409@naver.com>
2021-07-13 01:13:24 +0900

    make detail page style

--- lib/pages/post/detail_page.dart
+++ lib/pages/post/detail_page.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_blog/pages/post/home_page.dart';
+import 'package:flutter_blog/pages/post/update_page.dart';
+import 'package:get/get.dart';
 
 class DetailPage extends StatelessWidget {
   final int id;
@@ -10,8 +13,42 @@ class DetailPage extends StatelessWidget {
     // String data = Get.arguments;
     return Scaffold(
       appBar: AppBar(),
-      body: Center(
-        child: Text('Detail Page $id'),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              '글 제목!!',
+              style: TextStyle(fontWeight: FontWeight.bold, fontSize: 35),
+            ),
+            Divider(),
+            Row(
+              children: [
+                ElevatedButton(
+                  onPressed: () {
+                    Get.off(HomePage()); // 상태관리로 갱신 시킬 수 있음.
+                  },
+                  child: Text('삭제'),
+                ),
+                SizedBox(
+                  width: 10,
+                ),
+                ElevatedButton(
+                  onPressed: () {
+                    Get.to(UpdatePage());
+                  },
+                  child: Text('수정'),
+                ),
+              ],
+            ),
+            Expanded(
+              child: SingleChildScrollView(
+                child: Text('글 내용!!' * 500),
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
