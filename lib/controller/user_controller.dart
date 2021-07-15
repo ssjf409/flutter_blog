de6a8010f739adb79c413a39b238ccc02f57af40
Donghyeok Jang <ssjf409@naver.com>
2021-07-15 22:29:45 +0900

    DI

--- lib/controller/user_controller.dart
+++ lib/controller/user_controller.dart
@@ -4,11 +4,17 @@ import 'package:get/get.dart';
 
 class UserController extends GetxController {
   final UserRepository _userRepository = UserRepository();
+  final RxBool isLogin = false.obs; // UI가 관찰 가능한 변수 => 변경 => UI가 자동 업데이트
+
+  void test() {
+    isLogin.value = false;
+  }
 
   Future<String> login(String username, String password) async {
     String token = await _userRepository.login(username, password);
 
     if (token != '-1') {
+      isLogin.value = true; // isLogin(true);
       jwtToken = token;
       print('jwtToken : $jwtToken');
     }
--- lib/view/pages/post/home_page.dart
+++ lib/view/pages/post/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_blog/controller/user_controller.dart';
 import 'package:flutter_blog/size.dart';
 import 'package:flutter_blog/view/pages/post/detail_page.dart';
 import 'package:flutter_blog/view/pages/post/write_page.dart';
@@ -9,9 +10,14 @@ import 'package:get/get.dart';
 class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    // put 없으면 만들고, 있으면 찾기!!
+    UserController u = Get.find();
+
     return Scaffold(
       drawer: _navigation(context),
-      appBar: AppBar(),
+      appBar: AppBar(
+        title: Text('${u.isLogin}'),
+      ),
       body: ListView.separated(
         itemCount: 3,
         itemBuilder: (context, index) {
